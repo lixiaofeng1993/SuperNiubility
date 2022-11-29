71c904e97ee18d32f47bea275f6129dc36772c15
lixiaofeng1993 <15235514553@163.com>
2022-11-29 23:08:12 +0800

    gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -11,7 +11,7 @@ from multiprocessing import cpu_count
 bind = '127.0.0.1:8000'
 daemon = True  # 守护进程
 
-workers = cpu_count() * 2 + 1
+workers = cpu_count() * 2
 worker_class = 'gevent'
 forwarded_allow_ips = '*'
 
@@ -24,5 +24,5 @@ worker_connections = 65535
 # log
 capture_output = True
 loglevel = 'info'
-accesslog = "/tmp/EasyTest_access.log"  # 访问日志文件的路径
-errorlog = "/tmp/EasyTest_error.log"
+accesslog = "/www/wwwlogs/gunicorn_access.log"  # 访问日志文件的路径
+errorlog = "/www/wwwlogs/gunicorn_error.log"
