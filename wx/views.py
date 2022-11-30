ae81c4fd4b8519a91efc976771d8fbe4766a1f56
lixiaofeng1993 <15235514553@163.com>
2022-11-30 19:34:22 +0800

    wx

--- wx/views.py
+++ wx/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from django.utils.encoding import smart_str
 
 from public.wx_common import *
-from public.wx_message import Analysis
+from public.wx_message import parse_xml, Message
 
 
 class WechatServe(View):
@@ -24,53 +24,51 @@ class WechatServe(View):
 
     def post(self, request, *args, **kwargs):
         token = wx_login()
-        # try:
-        analysisObj = Analysis(smart_str(request.body))
-        toWxData = analysisObj.prase(smart_str(request.body))
-        return HttpResponse(smart_str(toWxData))
-        #     logger.info(f"======rec_msg========={rec_msg}")
-        #     if not rec_msg:
-        #         return HttpResponse('success')
-        #     to_user = rec_msg.FromUserName
-        #     from_user = rec_msg.ToUserName
-        #     text = rec_msg.Content
-        #     content, media_id, skip = "", "", ""
-        #     idiom = cache.get("IDIOM")
-        #     if text:
-        #         if text == "推荐":
-        #             content = cache.get("recommended-today")
-        #         elif text and (
-        #                 "DYNASTY" in text or "POETRY_TYPE" in text or "AUTHOR" in text or "RECOMMEND" in text
-        #         ):
-        #             skip = cache.get(text)
-        #             if not skip:
-        #                 content = "会话只有30分钟，想了解更多，请重新发起~"
-        #         elif not idiom and text in ["成语接龙", "接龙"]:
-        #             cache.sete(key=f"IDIOM", value=text, seconds=30 * 60)
-        #             content = "进入时效30分钟的成语接龙时刻，输入成语开始吧~"
-        #         elif not idiom and "IDIOM-INFO" in text:
-        #             content = "成语接龙会话时效只有30分钟，想了解更多，请重新发起~"
-        #         elif idiom and text == "exit":
-        #             cache.delete("IDIOM")
-        #             content = "See you later..."
-        #     if not content:
-        #         content, media_id = send_wx_msg(rec_msg, token, skip, idiom)
-        #     logger.info(f"======content========={content}")
-        #     if rec_msg.MsgType == 'text' and not media_id:
-        #         if "</a>" in content and len(content) >= 2000:
-        #             content = "..." + content[len(content) - 2000:]
-        #         elif len(content) >= 710 and "</a>" not in content:
-        #             content = content[:710] + "..."
-        #         return HttpResponse(
-        #             Message(to_user, from_user, content=content).send(),
-        #             media_type="application/xml")
-        #     elif rec_msg.MsgType == 'event' and content:
-        #         return HttpResponse(
-        #             Message(to_user, from_user, content=content).send(), media_type="application/xml")
-        #     elif rec_msg.MsgType == "image" or media_id:
-        #         return HttpResponse(
-        #             Message(to_user, from_user, media_id=media_id, msg_type="image").send(),
-        #             media_type="application/xml")
-        # except Exception as error:
-        #     logger.error(f"微信回复信息报错：{error}")
-        #     return HttpResponse('success')
+        try:
+            rec_msg = parse_xml(smart_str(request.body))
+            logger.info(f"======rec_msg========={rec_msg}")
+            if not rec_msg:
+                return HttpResponse('success')
+            to_user = rec_msg.FromUserName
+            from_user = rec_msg.ToUserName
+            text = rec_msg.Content
+            content, media_id, skip = "", "", ""
+            idiom = cache.get("IDIOM")
+            if text:
+                if text == "推荐":
+                    content = cache.get("recommended-today")
+                elif text and (
+                        "DYNASTY" in text or "POETRY_TYPE" in text or "AUTHOR" in text or "RECOMMEND" in text
+                ):
+                    skip = cache.get(text)
+                    if not skip:
+                        content = "会话只有30分钟，想了解更多，请重新发起~"
+                elif not idiom and text in ["成语接龙", "接龙"]:
+                    cache.sete(key=f"IDIOM", value=text, seconds=30 * 60)
+                    content = "进入时效30分钟的成语接龙时刻，输入成语开始吧~"
+                elif not idiom and "IDIOM-INFO" in text:
+                    content = "成语接龙会话时效只有30分钟，想了解更多，请重新发起~"
+                elif idiom and text == "exit":
+                    cache.delete("IDIOM")
+                    content = "See you later..."
+            if not content:
+                content, media_id = send_wx_msg(rec_msg, token, skip, idiom)
+            logger.info(f"======content========={content}")
+            if rec_msg.MsgType == 'text' and not media_id:
+                if "</a>" in content and len(content) >= 2000:
+                    content = "..." + content[len(content) - 2000:]
+                elif len(content) >= 710 and "</a>" not in content:
+                    content = content[:710] + "..."
+                return HttpResponse(
+                    Message(to_user, from_user, content=content).send(),
+                    media_type="application/xml")
+            elif rec_msg.MsgType == 'event' and content:
+                return HttpResponse(
+                    Message(to_user, from_user, content=content).send(), media_type="application/xml")
+            elif rec_msg.MsgType == "image" or media_id:
+                return HttpResponse(
+                    Message(to_user, from_user, media_id=media_id, msg_type="image").send(),
+                    media_type="application/xml")
+        except Exception as error:
+            logger.error(f"微信回复信息报错：{error}")
+            return HttpResponse('success')
