9862339d24f423253bcdf26343b1b7b33a8c0f95
lixiaofeng1993 <15235514553@163.com>
2022-11-30 19:26:17 +0800

    wx

--- public/wx_message.py
+++ public/wx_message.py
@@ -1,91 +1,65 @@
-#!/usr/bin/env python
-# _*_ coding: utf-8 _*_
-# 创 建 人: 李先生
-# 文 件 名: wx_message.py
-# 创建时间: 2022/11/27 0027 15:38
-# @Version：V 0.1
-# @desc :
+from xml.etree import ElementTree as ET
 import time
-import xml.etree.ElementTree as Et
-
-from public.log import logger
 
 
-def parse_xml(web_data):
-    logger.info(f"========web_data===========>>>{web_data}")
-    xml_data = Et.fromstring(web_data)
-    try:
-        msg_type = xml_data.find('MsgType').text
-    except Exception as error:
-        logger.error(f"解析微信XML消息，获取 MsgType 文案报错 ===>>> {error}")
-        return
-    if msg_type == 'text':
-        return TextMsg(xml_data)
-    elif msg_type == 'event':
-        return EventMsg(xml_data)
-    elif msg_type == "image":
-        return ImageMsg(xml_data)
+class ImageMsg:
+    def __init__(self, toUser, fromUser, mediaId):
+        self._toUser = toUser
+        self._fromUser = fromUser
+        self._rediaId = mediaId
+        self._nowTime = int(time.time())
+        self._mediaId = mediaId
+
+    def structReply(self):
+        text = """
+                <xml>
+                <ToUserName><![CDATA[{0}]]></ToUserName>
+                <FromUserName><![CDATA[{1}]]></FromUserName>
+                <CreateTime>{2}</CreateTime>
+                <MsgType><![CDATA[image]]></MsgType>
+                <Image>
+                <MediaId><![CDATA[{3}]]></MediaId>
+                </Image>
+                </xml>
+                """.format(self._fromUser, self._toUser, self._nowTime, self._mediaId)  # 前面两个参数的顺序需要特别注意
+        return text
 
 
-class Msg(object):
-    def __init__(self, xml_data):
-        self.ToUserName = xml_data.find('ToUserName').text
-        self.FromUserName = xml_data.find('FromUserName').text
-        self.CreateTime = xml_data.find('CreateTime').text
-        self.MsgType = xml_data.find('MsgType').text
+class Analysis:
+    def __init__(self, xmlData):
+        print("接收到的数据：" + xmlData)
+
+    def prase(self, xmlText):
+        xmlData = ET.fromstring(xmlText)
+        msgType = xmlData.find("MsgType").text
+        toUserName = xmlData.find("ToUserName").text
+        fromUserName = xmlData.find("FromUserName").text
+        if msgType == "text":
+            content = xmlData.find("Content").text
+            TextMsgObj = TextMsg(toUserName, fromUserName, content)
+            return TextMsgObj.structReply()
+        elif msgType == "image":
+            mediaId = xmlData.find("MediaId").text
+            ImageMsgObj = ImageMsg(toUserName, fromUserName, mediaId)
+            return ImageMsgObj.structReply()
 
 
-class TextMsg(Msg):
-    def __init__(self, xml_data):
-        Msg.__init__(self, xml_data)
-        self.Content = xml_data.find('Content').text
+class TextMsg:
+    def __init__(self, toUser, fromUser, recvMsg):
+        self._toUser = toUser
+        self._fromUser = fromUser
+        self._recvMsg = recvMsg
+        self._nowTime = int(time.time())
 
-
-class EventMsg(Msg):
-    def __init__(self, xml_data):
-        Msg.__init__(self, xml_data)
-        self.Content = xml_data.find('EventKey').text
-        self.Event = xml_data.find('Event').text
-
-
-class ImageMsg(Msg):
-    def __init__(self, xml_data):
-        Msg.__init__(self, xml_data)
-        self.MediaId = xml_data.find('MediaId').text
-
-
-class Message:
-    def __init__(self, to_user, from_user, content="", media_id="", msg_type="text"):
-        self.to_user = to_user
-        self.from_user = from_user
-        self.content = content
-        self.media_id = media_id
-        self.msg_type = msg_type
-        self.message = ""
-
-    def send(self):
-        if self.msg_type == "text":
-            self.message = f"""
-                        <xml>
-                        <ToUserName><![CDATA[{self.to_user}]]></ToUserName>
-                        <FromUserName><![CDATA[{self.from_user}]]></FromUserName>
-                        <CreateTime>{int(time.time())}</CreateTime>
-                        <MsgType><![CDATA[text]]></MsgType>
-                        <Content><![CDATA[{self.content}]]></Content>
-                        </xml>
-                   """
-            logger.info(f"微信回复消息文案长度 {len(self.content)}")
-        elif self.msg_type == "image":
-            self.message = f"""
-                    <xml>
-                      <ToUserName><![CDATA[{self.to_user}]]></ToUserName>
-                      <FromUserName><![CDATA[{self.from_user}]]></FromUserName>
-                      <CreateTime>{int(time.time())}</CreateTime>
-                      <MsgType><![CDATA[image]]></MsgType>
-                      <Image>
-                        <MediaId><![CDATA[{self.media_id}]]></MediaId>
-                      </Image>
-                    </xml>
-                    """
-            logger.info(f"微信回复图片 {self.media_id}")
-        return self.message
+    def structReply(self):
+        content = self._recvMsg
+        text = """
+                <xml>
+                <ToUserName><![CDATA[{0}]]></ToUserName>
+                <FromUserName><![CDATA[{1}]]></FromUserName>
+                <CreateTime>{2}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[{3}]]></Content>
+                </xml>
+                """.format(self._fromUser, self._toUser, self._nowTime, content)  # 前面两个参数的顺序需要特别注意
+        return text
