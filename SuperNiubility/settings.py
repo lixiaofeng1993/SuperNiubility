21acef2706f86c885b0321546e5f619074da97d2
lixiaofeng1993 <15235514553@163.com>
2022-11-30 13:11:56 +0800

    图片

--- SuperNiubility/settings.py
+++ SuperNiubility/settings.py
@@ -144,7 +144,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # 关闭浏览器，则COOKIE失效
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_ROOT = "/www/wwwroot/static/"
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
