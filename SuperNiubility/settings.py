cdb7f78e5bd205962dd65c4aa1625b8638348c45
lixiaofeng1993 <15235514553@163.com>
2022-11-30 13:34:10 +0800

    mysql

--- SuperNiubility/settings.py
+++ SuperNiubility/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
         'NAME': 'nb',
         'USER': "root" if platform.system() == 'Windows' else "nb",
         'PASSWORD': '123456',
