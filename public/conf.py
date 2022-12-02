b3ab62573144473284814a5794c880bf33c7a3a8
lixiaofeng1993 <15235514553@163.com>
2022-12-02 12:53:14 +0800

    问题

--- public/conf.py
+++ public/conf.py
@@ -6,7 +6,7 @@
 # @Version：V 0.1
 # @desc :
 from django.conf import settings
-from datetime import datetime
+from datetime import date, datetime
 
 # token
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
